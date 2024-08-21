06e3faf0d40160d366a2bfff7065ab6a89c0cb64
kit bagares <kityoubagares94@gmail.com>
2024-08-22 07:38:08 +0800

    Update next confg export images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio-kit', // Set this to your repository name
+  assetPrefix: '/portfolio-kit/', // Ensures assets are served from the correct path
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  trailingSlash: true, // Ensures trailing slashes in URLs, which can help with routing
+};
 
 export default nextConfig;
