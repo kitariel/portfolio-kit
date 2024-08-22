6f85d6c8b005d2f77338b8b4b0afb1e3d42fb417
kit bagares <kityoubagares94@gmail.com>
2024-08-22 08:02:59 +0800

    Static

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enables static export
+  basePath: '/portfolio-kit', // Your GitHub repository name
+  assetPrefix: '/portfolio-kit/', // Ensures assets are served from the correct path
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  trailingSlash: true, // Adds a trailing slash to all routes
+};
 
 export default nextConfig;
